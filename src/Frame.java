0e35c69cc4d56fbb5579bdfe4bccb5f1b28c6797
Bryce Butenhof <butenhof@uwm.edu>
2017-02-01 19:01:41 -0600

    Frame class imported

--- src/Frame.java
+++ src/Frame.java
@@ -1,10 +1,18 @@
-
 public class Frame {
-	private int t1;
-	private int t2;
-	private boolean isStrike;
-	private boolean isSpare;
-}
-{
-	//add some tests
+    private int throw1;
+    private int throw2;
+    
+    public Frame(){
+        throw1 = 0;
+        throw2 = 0;
+    }
+    
+    public boolean isStrike(){ return throw1==10;}
+    public boolean isSpare(){ return (throw1 + throw2)==10;}
+    
+    public int getThrow1(){ return throw1;}
+    public int getThrow2(){ return throw2;}
+    
+    public void setThrow1(int throw1){ this.throw1 = throw1;}
+    public void setThrow2(int throw2){ this.throw2 = throw2;}
 }
